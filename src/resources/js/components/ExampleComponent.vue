321f80a2c05e50117ee49c056de3b0305d6fb66b
Pronesti <dieh.diego@gmail.com>
2020-11-18 16:18:40 -0300

    tracked src files

---
+++ src/resources/js/components/ExampleComponent.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Example Component</div>
+
+                    <div class="card-body">
+                        {{showExample}}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['propito'],
+        computed: {
+            showExample(){
+                return this.propito;
+            }
+        },
+        mounted() {
+            console.log('Component mounted.')
+            console.log(this.propito)
+        },
+        created(){
+            console.log(this.propito)
+        }
+    }
+</script>
